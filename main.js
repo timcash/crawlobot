bd76805b4f1c317219d6d8167763bc91b976e6c6
Tim <timcash@gmail.com>
2020-08-08 21:23:21 -0600

    start small

--- main.js
+++ main.js
@@ -2,17 +2,19 @@ const { webkit } = require("playwright");
 // const fs = require("fs");
 // const myArgs = process.argv.slice(2);
 
-(async () => {
-  const browser = await webkit.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
+// (async () => {
+//   const browser = await webkit.launch();
+//   const context = await browser.newContext();
+//   const page = await context.newPage();
 
-  // Log and continue all network requests
-  page.route("**", (route) => {
-    console.log(route.request().url());
-    route.continue();
-  });
+//   // Log and continue all network requests
+//   page.route("**", (route) => {
+//     console.log(route.request().url());
+//     route.continue();
+//   });
 
-  await page.goto("https://google.com");
-  await browser.close();
-})();
+//   await page.goto("https://google.com");
+//   await browser.close();
+// })();
+
+console.log("hello");
