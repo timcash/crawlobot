7d69ae628c76dc822411705ae195e8c5d7a4e92a
Tim <timcash@gmail.com>
2020-08-08 21:35:06 -0600

    try with npm

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 const { webkit } = require("playwright");
-// const fs = require("fs");
-// const myArgs = process.argv.slice(2);
+const fs = require("fs");
+const myArgs = process.argv.slice(2);
 
-// (async () => {
-//   const browser = await webkit.launch();
-//   const context = await browser.newContext();
-//   const page = await context.newPage();
+(async () => {
+  const browser = await webkit.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-//   // Log and continue all network requests
-//   page.route("**", (route) => {
-//     console.log(route.request().url());
-//     route.continue();
-//   });
+  // Log and continue all network requests
+  page.route("**", (route) => {
+    console.log(route.request().url());
+    route.continue();
+  });
 
-//   await page.goto("https://google.com");
-//   await browser.close();
-// })();
-
-console.log("hello");
+  await page.goto(myArgs[0]);
+  await browser.close();
+})();
